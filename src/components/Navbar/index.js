4dd6b869c3acc08d357304472939f0ba77c6a0a5
Nguyen Long <long.nguyenhoang7996@gmail.com>
2022-07-20 17:59:56 +0700

    navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,26 +5,30 @@ import Select from '../base/Select.js';
 function Navbar({ menu }) {
     return html`
         <div class='navbar'>
-            <div class='introduce'>
+            <div class='navbar__introduce'>
                 <p>Italio - Nhà sản xuất kem chuyên cung cấp kem Ý, kem cao cấp, máy làm kem,...</p>
-                <a href='https://https://italio.vn/'>Dùng thử miễn phí</a>
+                <div class='navbar__introduce--try-free'>
+                    <a href='https://italio.vn/'>Dùng Thử Miễn Phí</a>
+                </div>
             </div>
-            <div class='menu'>
-                ${menu.map(i => {
-                    let children = [];
-                    if (i.children && i.children.length) {
-                        children = [...i.children];
-                    }
-
-                    return `
-                    <a href=${i.link}>
-                        ${i.title}
-                    </a>
-                    ${children.map(c => Select(c))}
-                `})}
+            <div class='navbar__bar'>
+                <img class='navbar__bar--logo' src='https://italio.vn/wp-content/uploads/2021/05/logooo-90x55.png'></img>
+                <div class='menu'>
+                    ${(menu || []).map(i => html`
+                        <div ${i.id ? `title=${i.id}` : ''} class='menu__item'>
+                            <a href=${i.link || '#'} class='menu__item--link'>
+                                ${i.title}
+                            </a>
+                            ${i.children && i.children.length
+                                && i.children.map(c => Select({ ...c, level: 1 }))
+                            }
+                        </div>
+                    `)}
+                </div>
             </div>
         </div>
     `;
 }
+// ${i.children && i.children.length && Children(i.children, 1)}
 
 export default connect()(Navbar);
